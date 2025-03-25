076c210e23c4d4eb85cc298094d3781555a0feeb
JChoy <jchoyeclipse@gmail.com>
2025-03-26 03:45:39 +0900

    feat: change template script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -3,34 +3,27 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {IDiamond} from "diamond/interfaces/IDiamond.sol";
-
+import {DiamondScript} from "diamond/helpers/DiamondScript.sol";
 import {ICounter} from "../src/interfaces/ICounter.sol";
 import {ICounterApp} from "../src/interfaces/ICounterApp.sol";
 import {CounterApp} from "../src/CounterApp.sol";
 import {CounterFacet} from "../src/facets/CounterFacet.sol";
 
-contract CounterScript is Script {
-    ICounterApp public counter;
+contract CounterScript is DiamondScript("CounterApp") {
+    string[] facetNames;
+    bytes[] facetArgs;
+
+    ICounterApp counter;
 
     function setUp() public {}
 
-    function run() public {
+    function deploy() public {
         vm.startBroadcast();
 
-        counter = ICounterApp(address(new CounterApp(address(this))));
+        facetNames.push("CounterFacet");
+        facetArgs.push("");
 
-        CounterFacet counterFacet = new CounterFacet();
-        IDiamond.FacetCut[] memory cut = new IDiamond.FacetCut[](1);
-        bytes4[] memory functionSelectors = new bytes4[](3);
-        functionSelectors[0] = ICounter.number.selector;
-        functionSelectors[1] = ICounter.setNumber.selector;
-        functionSelectors[2] = ICounter.increment.selector;
-        cut[0] = IDiamond.FacetCut({
-            facetAddress: address(counterFacet),
-            action: IDiamond.FacetCutAction.Add,
-            functionSelectors: functionSelectors
-        });
-        counter.diamondCut(cut, address(0), "");
+        counter = ICounterApp(deploy(abi.encode(address(this)), facetNames, facetArgs));
 
         vm.stopBroadcast();
     }
